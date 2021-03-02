08d65444bff1b0ec352aa83b930188cf29f9c7f8
dhananjaypatil707 <dhananjaypatil707@gmail.com>
2021-03-02 17:17:19 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+/*
 function showData(data) {
   return data.toUpperCase();
 }
 function Welcome() {
   return <h2>This is from component,{showData("hello")}</h2>;
-}
-
+}*/
 const myname = "Dhananjay";
-const element = <h2>Hello,{showData(myname)}</h2>;
-ReactDOM.render(<Welcome />, document.getElementById("root"));
+const element = <h2>Hello,{myname}</h2>;
+ReactDOM.render(<element />, document.getElementById("root"));
