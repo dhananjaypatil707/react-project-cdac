c687bbd2d077bbcc7fed31947c85d664cb8a19ab
dhananjaypatil707 <dhananjaypatil707@gmail.com>
2021-03-02 17:06:08 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from "react-dom";
 function showData(data) {
   return data.toUpperCase();
 }
+function Welcome() {
+  return <h2>This is from component,{showData("hello")}</h2>;
+}
 
 const myname = "Dhananjay";
 const element = <h2>Hello,{showData(myname)}</h2>;
-ReactDOM.render(<showData />, document.getElementById("root"));
+ReactDOM.render(<Welcome />, document.getElementById("root"));
